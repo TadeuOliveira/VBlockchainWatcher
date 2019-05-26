e80190efefe0733ab6fb29c45dc53386e05b7a88
Tadeu <tadeu_oliveira@id.uff.br>
2019-05-26 03:18:13 -0300

    routes working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello App!</h1>
+
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+      <router-link to="/blocks">Go to Blocks page</router-link>
+      <hr>
+      <router-link to="/">Go to Home page</router-link>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
---
+++ src/components/Blocks.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="blocks">
+	<h1>This is blocks page</h1>
+	<p>This is a paragraph, I hope it shows</p>		
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Blocks',
+  props: {
+    msg: String
+  }
+}
+</script>
